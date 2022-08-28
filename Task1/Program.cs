60c25fc315ab562b6caa65ad35f25b6d532f5086
Olga Korchagina <olga_korchagina_2016@mail.ru>
2022-08-28 16:18:50 +0300

    решена задача с заменой символов в тексте

---
+++ Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿// заменить все пробелы в тексте черточками,
+// маленькие буквы к заменить большими К,
+// большие С замеинть маленькими с
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что, "
++ "ежели бы вас послали вместо нашего милого Винценгероде,"
++ "вы бы взяли приступом согласие прусского короля. "
++ "Вы так красноречивы. Вы дадите мне чаю?";
+
+//string s = "qwerty"
+//            012
+// s[3] //r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int lenght = text.Length;
+    for(int i = 0; i < lenght; i++)
+    {
+        if(text [i]== oldValue) result = result +$"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+}
+
+string newText = Replace(text, ' ', '_');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'с', 'С');
+Console.WriteLine(newText);
+Console.WriteLine();
